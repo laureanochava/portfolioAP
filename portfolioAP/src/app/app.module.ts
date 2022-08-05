502a5265726fec550686657c02ed3a8e57cfcc25
laureano <LaureanoCuentas@outlook.com>
2022-08-05 16:08:45 -0300

    Tercer Commit
    Header con banner e iconos de redes sociales + instalacion de bootstrap al proyecto

--- portfolioAP/src/app/app.module.ts
+++ portfolioAP/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Componentes/header/header.component';
+import { LogoAPComponent } from './Componentes/logo-ap/logo-ap.component';
+import { RedesComponent } from './Componentes/redes/redes.component';
+import { BannerComponent } from './Componentes/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    RedesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
